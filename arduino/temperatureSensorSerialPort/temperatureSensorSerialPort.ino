b0e2f18becd0cfe066317f11a6ca708fb4e992a3
Daniel R. Torres Ruiz <danielr.torresruiz@icloud.com>
2018-02-28 14:41:00 +0100

    Deleting useless comments

--- arduino/temperatureSensorSerialPort/temperatureSensorSerialPort.ino
+++ arduino/temperatureSensorSerialPort/temperatureSensorSerialPort.ino
@@ -5,40 +5,6 @@ void setup() {
 }
 
 void loop() {
-//   Serial.println("530");
-//   delay(1000);
-//   Serial.println("531");
-//   delay(1000);
-//   Serial.println("533");
-//  delay(1000);
-//  Serial.println("533");
-//  delay(1000);
-//  Serial.println("533");
-//  delay(1000);
-//  Serial.println("530");
-//  delay(1000);
-//  Serial.println("531");
-//  delay(1000);
-//  Serial.println("533");
-//  delay(1000);
-//  Serial.println("533");
-//  delay(1000);
-//  Serial.println("533");
-//  delay(1000);
-//  Serial.println("530");
-//  delay(1000);
-//  Serial.println("531");
-//  delay(1000);
-//  Serial.println("533");
-//  delay(1000);
-//  Serial.println("533");
-//  delay(1000);
-//  Serial.println("539");
-//  delay(1000);
-//  // Para combrar que no explote con un valor distinto de un número
-//  Serial.println("Hola jejeplot"); 
-//  delay(1000);
-
   int temp_reading = analogRead(pinTemp);
   Serial.println(temp_reading);
   delay(1000);
